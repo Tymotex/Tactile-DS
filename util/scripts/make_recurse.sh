d3c5aea18161261d32867afe7fd5720f47c6baa9
Tim Zhang <admin@timz.dev>
2024-05-31 07:34:01 +1000

    Clean up script styling and error handling.

--- util/scripts/make_recurse.sh
+++ util/scripts/make_recurse.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
-# Finds and runs all makefiles in the target directory and its subdirectories
+# Finds and runs all MAKEFILES in the target directory and its subdirectories
 # Usage: ./make_recurse <target directory> [make options]  
 
-targetDirectory=$1
-makeArg=$2
-makefiles=$(find $targetDirectory | grep -E 'Makefile$')
-cwd=$(pwd)
-for makefile in $makefiles; do
-    makeDir=$(echo $makefile | sed 's/\/Makefile$//g')
-    echo "Running 'make $makeArg' in: $makeDir"
-    cd $cwd/$makeDir
-    if make $makeArg > /dev/null 2> /dev/null; then
+TARGET_DIR=$1
+MAKE_ARG=$2
+MAKEFILES=$(find "$TARGET_DIR" | grep -E 'Makefile$')
+CWD=$(pwd)
+
+for makefile in $MAKEFILES; do
+    make_dir=$(echo "$makefile" | sed 's/\/Makefile$//g')
+    echo "Running 'make $MAKE_ARG' in: $make_dir"
+
+    make_dir="$CWD/$make_dir"
+    if [ ! -d "$make_dir" ]; then
+        echo "$DIRECTORY does not exist."
+        exit 1
+    fi
+    cd "$make_dir" || exit 1
+
+    if make "$MAKE_ARG" > /dev/null 2> /dev/null; then
         echo " → Succeeded"
     else 
         echo " → Failed"
